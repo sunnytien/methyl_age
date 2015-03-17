1644638fcc41893733045f63d3a1f62e4d30521f
Katherine Siddle <kjsiddle@zeus.geh.pasteur.fr>
2015-03-17 14:27:58 +0100

    SQL-based code for PCA

--- pca.R
+++ pca.R
@@ -1,15 +1,11 @@
-library("pcaMethods")
 library("dplyr")
 
 db = src_sqlite("./data/BMIQ.db")
 beta = tbl(db, "test4")
 
 sites = beta %>%
-  group_by(probe.id) %>%
-  summarize(n=n()) %>%
-  ungroup %>%
-  filter(n==max(n)) %>%
-  sample_n(1000)
+  group_by(gsm.id) %>%
+  summarize(n=n()) 
 
 beta.subsample = beta %>%
   semi_join(sites)
