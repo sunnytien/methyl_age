8bd193163c83b3fda21b066e3c00e6b159076423
Katherine Siddle <kjsiddle@zeus.geh.pasteur.fr>
2015-03-17 15:08:57 +0100

    added linked plots to PCA scatter

--- pca.R
+++ pca.R
@@ -1,12 +1,29 @@
 library("dplyr")
+library("tidyr")
 
 db = src_sqlite("./data/BMIQ.db")
 beta = tbl(db, "test4")
 
 sites = beta %>%
-  group_by(gsm.id) %>%
-  summarize(n=n()) 
+  group_by(probe.id) %>%
+  summarize(n=n()) %>%
+  collect
+
+sites.sample = sites %>%
+  filter(n == max(n)) %>%
+  sample_n(10) %>%
+  select(probe.id)
 
 beta.subsample = beta %>%
-  semi_join(sites)
+  semi_join(sites.sample, copy=T) %>%
+  collect %>%
+  spread(probe.id, beta) 
 
+x = beta.subsample %>%
+  select(starts_with("cg")) %>%
+  as.matrix
+rownames(x) = beta.subsample$gsm.id
+
+p = pca(x, scale="none", center=T)
+
+save(p, file="./data/pca.Rdata")
