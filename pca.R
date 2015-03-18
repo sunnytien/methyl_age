886e2910705d48d5553c58764ee6ffb95c9a5d28
Katherine Siddle <kjsiddle@zeus.geh.pasteur.fr>
2015-03-18 12:04:19 +0100

    changes to sql pipeline

--- pca.R
+++ pca.R
@@ -5,14 +5,12 @@ db = src_sqlite("./data/BMIQ.db")
 beta = tbl(db, "test4")
 
 sites = beta %>%
-  group_by(probe.id) %>%
-  summarize(n=n()) %>%
-  collect
+  select(probe.id) %>%
+  distinct %>%
+  head(1000)
 
 sites.sample = sites %>%
-  filter(n == max(n)) %>%
-  sample_n(10) %>%
-  select(probe.id)
+  sample_n(1000) 
 
 beta.subsample = beta %>%
   semi_join(sites.sample, copy=T) %>%
