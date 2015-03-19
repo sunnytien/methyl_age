c44eda31786d561a9710955d8d512a5d9f23ee9d
Katherine Siddle <kjsiddle@zeus.geh.pasteur.fr>
2015-03-19 15:08:31 +0100

    separating splitter functions from script

--- splitter.R
+++ splitter.R
@@ -1,45 +1,17 @@
 library("BatchJobs")
-library("minfi")
-library("dplyr")
-library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
-data(IlluminaHumanMethylation450kanno.ilmn12.hg19)
 
-process.mset = function(filename){
-  
-  base = paste(gsub("meth_matrices.Rdata$", "", filename), "GSM/", sep="")
-  if(!file.exists(base)) system(paste("mkdir", base))
-  
-  get.path = function(id) paste(base, id, ".Rdata", sep="")
-  
-  load(filename)
-  
-  betas = meth / (meth + unmeth + 100) %>%
-    as.data.frame
-  
-  lapply(names(betas), function(id){
-    b = betas[[id]]
-    names(b) = rownames(betas)
-    save(b, file=get.path(id))
-  })
-  
-  return(rownames(meth))
-}
+source("splitter_funcs.R")
 
-files = list.files("~/data/methyl_age/GEO",
-                   full.names=T,
-                   recursive=T) %>%
-  (function(x) grep("/meth_matrices.Rdata$", x, value=T))
+gses = list.files(DATA_DIR) %>%
+  grep("GSE", ., value=T)
 
-reg = makeRegistry("splitting", packages=c("dplyr", "minfi"))
-batchMap(reg, process.mset, files)
-submitJobs(reg)
+reg = makeRegistry("splitter2", 
+                   packages=c("dplyr", 
+                              "IlluminaHumanMethylation450kanno.ilmn12.hg19",          
+                              "tidyr", 
+                              "data.table", 
+                              "stringr"),
+                   src.files="splitter_funcs.R")
 
-waitForJobs(reg)
-
-annotated.sites = rownames(IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Manifest)
-
-sites = loadResults(reg)
-
-unannotated.sites = lapply(sites, function(x) x[!(x %in% annotated.sites)]) %>%
-  unlist %>%
-  unique
+batchMap(reg, splitter, gses)
+submitJobs(reg,10)
---
+++ splitter_funcs.R
@@ -0,0 +1,258 @@
+## rewriting splitting function to be generalized
+## idea is for it to be broadly applicable do all GSE sets
+
+library("dplyr")
+library('stringr')
+library("GEOquery")
+library("data.table")
+library("tidyr")
+library("IlluminaHumanMethylation450kanno.ilmn12.hg19")
+library("BatchJobs")
+
+#DATA_DIR = "./data/"
+DATA_DIR = "./"
+
+data(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+
+get.beta = function(gse.id){
+  cat(paste("Getting beta-values for", gse.id, "\n"))
+  
+  if(file.exists(paste(gse.id, "/", "idat", sep=""))) get.beta.idat(gse.id) else
+    get.beta.notidat(gse.id)
+}
+
+get.beta.idat = function(gse.id){
+  
+  filename = paste(DATA_DIR, gse.id, "/idat", sep="")
+  cat(paste("Found idat folder at:", filename, "\n"))
+
+  rg = read.450k.exp(filename, verbose=T)
+  meth = preprocessIllumina(rg, bg.correct=T, normalize="no")
+  
+  beta = getBeta(meth, offset=100)
+  
+}
+
+
+get.beta.notidat = function(gse.id){
+  
+  ag = function(x) any(grepl(x, colnames(d), ignore.case=T))
+  
+  filename = gse.id  %>%
+    paste(DATA_DIR, ., sep="") %>%
+    list.files(full.names=T) %>%
+    grep(paste(gse.id, gse.id, sep="/"), ., value=T) %>%
+    grep("series_matrix", ., value=T, invert=T)
+  
+  if(length(filename) != 1) stop("Could not locate datafile")
+  
+  cat(paste("Found possible datafile at:", filename, "\n"))
+  
+  d = fread(filename)
+  
+  if(ag("meth")) return(get.beta.meth(d))
+  if(ag("signal")) return(get.beta.signal(d))
+  stop("Did not detect filetype\n")
+}
+
+get.beta.meth = function(d){
+  
+  meth = d %>%
+    select(contains("meth", ignore.case=T)) %>%
+    select(-contains("unmeth", ignore.case=T)) %>%
+    as.matrix %>%
+    apply(2, as.numeric)
+  
+  unmeth = d %>%
+    select(contains("unmeth", ignore.case=T)) %>%
+    as.matrix %>%
+    apply(2, as.numeric)
+  
+  if(any(dim(meth) != dim(unmeth))) stop("meth and unmeth matrices have different dimensions")
+  
+  colnames(meth) = gsub(".Methylat.+$", "", colnames(meth), ignore.case=T, perl=T)
+  colnames(unmeth) = gsub(".Unmethyl.+$", "", colnames(meth), ignore.case=T, perl=T)
+  
+  if(any(colnames(meth) != colnames(unmeth))) stop("Different column names for meth and unmeth")
+  
+  beta = meth / (meth + unmeth + 100)
+  colnames(beta) = colnames(meth)
+  
+  id.column = grep("ID", colnames(d), ignore.case=T, value=T) %>%
+    grep("REF|Target", ., ignore.case=T, value=T)
+  
+  ids = d %>%
+    select(contains(id.column)) %>%
+    as.data.frame
+  
+  rownames(beta) = ids[[1]]
+  return(beta)
+  
+}
+
+get.beta.signal = function(d){
+  signal = d %>%
+    select(contains("signal", ignore.case=T))
+  
+  # methylated is "Signal B"
+  meth = signal %>%
+    select(ends_with("B", ignore.case=T)) %>%
+    as.matrix %>%
+    apply(2, as.numeric)
+  
+  # unmethylated is "Signal A"
+  unmeth = signal %>% 
+    select(-ends_with("B", ignore.case=T)) %>% 
+    as.matrix %>%
+    apply(2, as.numeric)
+  
+  if(any(dim(meth) != dim(unmeth))) stop("meth and unmeth matrices have different dimensions")
+  
+  colnames(meth) = gsub(".signal.+$", "", colnames(meth), ignore.case=T, perl=T)
+  colnames(unmeth) = gsub(".signal.+$", "", colnames(meth), ignore.case=T, perl=T)
+  
+  if(any(colnames(meth) != colnames(unmeth))) warn("Different column names for meth and unmeth")
+  
+  beta = meth / (meth + unmeth + 100)
+  colnames(beta) = colnames(meth)
+  
+  id.column = grep("ID", colnames(d), ignore.case=T, value=T) %>%
+    grep("REF|Target|geneInfo", ., ignore.case=T, value=T)
+  
+  ids = d %>%
+    select(contains(id.column)) %>%
+    as.data.frame
+
+  if(ncol(ids) > 1) warn("Multiple ID columns found\n")
+  
+  rownames(beta) = ids[[1]]
+  return(beta)
+}
+
+write.beta = function(gse.id, beta){
+  write.probe = function(id){
+    
+    probe.file = paste(DATA_DIR, gse.id, "/GSM/", id, ".Rdata", sep="")
+    if(!file.exists(probe.file)){
+      x = beta[,id]
+      names(x) = rownames(beta)
+    
+      save(x, file=probe.file)
+    }
+    return(T)
+  }
+  
+  sapply(colnames(beta), write.probe)
+}
+
+write.mapping = function(gse.id, mapping){
+  mapping.file = paste(DATA_DIR, gse.id, "/GSM/mapping.Rdata", sep="")
+  save(mapping, file=mapping.file)
+}
+
+splitter = function(gse.id){
+  cat(paste("Initiating splitting of", gse.id, "\n"))
+  
+  beta = get.beta(gse.id)
+  write.beta(gse.id, beta)
+  
+  mapping = map.to.gse(gse.id, beta)
+  write.mapping(gse.id, mapping)
+}
+
+map.to.gse = function(gse.id, beta){
+  
+  if(file.exists(paste(DATA_DIR, gse.id, "/GSM/mapping.Rdata", sep=""))){
+    return(get(load(paste(DATA_DIR, gse.id, "/GSM/mapping.Rdata", sep=""))))
+  }
+  
+  if(all(grepl("GSM[0-9]+", colnames(beta)))) return(map.to.gse.simple(beta))
+  
+  if(file.exists(paste(DATA_DIR,
+                       gse.id, 
+                       "/", 
+                       gse.id, 
+                       "_series_matrix.txt", 
+                       sep=""))) return(map.to.gse.cor(gse.id, beta))
+  
+  return(NA)
+}
+
+map.to.gse.simple = function(beta){
+  cat("Looking like ids are already GSM ids\n")
+  find.id = function(x) str_match(x, "GSM[0-9][0-9][0-9][0-9][0-9][0-9][0-9]")[1]
+  data.frame(raw.id=colnames(beta),
+             normed.id=sapply(colnames(beta), find.id),
+             r=NA,
+             stringsAsFactors=F)
+}
+
+clean.series.file = function(series.file){
+  cleaned.file = paste(series.file, "cleaned", sep="_")
+  
+  if(!file.exists(cleaned.file)){
+    cmd = paste("cat ", series.file, " | grep -v ! >", cleaned.file, sep="")
+    system(cmd)
+  }
+  cleaned.file
+}
+
+map.to.gse.cor = function(gse.id, beta){
+  
+  cat("Mapping via correlation\n")
+  
+  type1 = IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Manifest %>%
+    as.data.frame %>%
+    mutate(probe.id=row.names(.)) %>%
+    filter(Type=="I") %>%
+    {.$probe.id}
+  
+  series.file = list.files(paste(DATA_DIR, gse.id, sep=""), full.names=T) %>%
+    grep("series_matrix", ., value=T) %>%
+    grep("txt$", ., value=T) %>%
+    clean.series.file
+  
+  d = fread(series.file)
+  
+  beta.normed = d %>%
+    select(starts_with("GSM")) %>%
+    as.matrix
+  
+  ids = d %>%
+    select(contains("ID")) %>%
+    as.data.frame
+  
+  rownames(beta.normed) = ids[[1]]
+  
+  sites = intersect(rownames(beta.normed), rownames(beta)) %>%
+    {
+      .[. %in% type1]
+    } %>%
+    sample(5000)
+  
+  correlations = cor(beta[sites, ], beta.normed[sites, ], method="spearman", use="pair") %>%
+    as.data.frame %>%
+    mutate(raw.id=row.names(.)) %>%
+    gather(normed.id, r, -raw.id) %>%
+    group_by(raw.id) %>%
+    filter(r == max(r)) %>%
+    ungroup
+  
+  n.normed = correlations %>%
+    select(normed.id) %>%
+    distinct %>%
+    nrow
+  
+  n.raw = correlations %>%
+    select(raw.id) %>%
+    distinct %>%
+    nrow
+  
+  if(n.normed < n.raw) stop("Multiple raw samples mapping to same normalized sample\n")
+  if(min(correlations$r) < 0.95) stop("Poor correlation found in samples\n")
+  
+  return(correlations)
+}
+
+
+
