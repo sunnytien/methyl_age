4af80709465c1d3c047220d02b8125e2d9923982
Eric Kramer <ekramer@zeus.geh.pasteur.fr>
2015-03-24 17:23:09 +0100

    separating out mapper functions

---
+++ preprocessing/mapper_funcs.R
@@ -0,0 +1,67 @@
+get.header = function(gse.id){
+  matrix.file = list.files(paste(DATA_DIR, gse.id, sep=""), full.names=T) %>%
+    grep("series_matrix.txt$", ., value=T) 
+  
+  if(length(matrix.file) == 0) stop("No series matrix file found")
+  if(length(matrix.file) > 1) stop("Multiple series matrix files found")
+  
+  matrix.file %>%
+    file %>%
+    readLines(200) %>%
+    grep("^!|ID.REF", ., value=T)
+}
+
+gse.mapper = function(gse.id){
+  
+  require("dplyr")
+  require("stringr")
+  
+  gse.id = str_match(filename, "GSE[0-9]+")[1,1]
+  
+  id = gsub("^.+/GSE[0-9]+_", "", filename) %>%
+    gsub(".Rdata$", "", .)
+  
+  header = get.header(gse.id)
+  
+  ## first try finding exact match
+  f = if(any(grepl(id, header))){ 
+    grep
+  } else if(any(agrepl(id, header))){ # then try fuzzy match
+    agrep
+  } else{
+    stop(paste("Could not find id for: ", gse.id, id, "\n"))
+  }
+  
+  id.line = f(id, header, value=T)
+  gsm.line = grep("ID.REF", header, value=T)
+  
+  ids = id.line %>%
+    gsub("\"", "", .) %>%
+    gsub("\'", "", .) %>%
+    strsplit("\t") %>%
+    .[[1]]
+  
+  gsms = gsm.line %>%
+    gsub("\"", "", .) %>%
+    gsub("\'", "", .) %>%
+    strsplit("\t") %>%
+    .[[1]]
+  
+  ind = f(id, ids)
+  
+  ## trying to rescue a multiple match here
+  ## GSM10 and GSM100 should resolve uniquely
+  ## after this statement
+  if(length(ind) > 1){
+    ind = f(paste(id, "[^0-9]", sep=""), ids)
+  }
+  
+  if(length(ind) > 1) stop("Multiple matches for ID found")
+  if(length(ind) == 0) stop("No matches for ID found")
+  
+  data.frame(series.id=gse.id,
+             gsm.id=gsms[[ind]],
+             raw.id=id,
+             stringsAsFactors=F)
+  
+}
--- preprocessing/mappers.R
+++ preprocessing/mappers.R
@@ -1,50 +1,8 @@
-get.header = function(gse.id){
-  list.files(paste(DATA_DIR, gse.id, sep=""), full.names=T) %>%
-    grep("series_matrix", ., value=T) %>%
-    file %>%
-    readLines(200) %>%
-    grep("^!|ID.REF", ., value=T)
-}
+library("BatchJobs")
 
-gse.mapper = function(gse.id){
-  
-  gse.id = str_match(filename, "GSE[0-9]+")[1,1]
-  
-  id = gsub("^.+/GSE[0-9]+_", "", filename) %>%
-    gsub(".Rdata$", "", .)
-  
-  header = get.header(gse.id)
-  
-  ## first try finding exact match
-  f = if(any(grepl(id, header))){ 
-    grep
-  } else if(any(agrepl(id, header))){
-    agrep
-  } else{
-    stop(paste("Could not find id for: ", gse.id, id, "\n"))
-  }
-  
-  id.line = f(id, header, value=T)
-  gsm.line = grep("ID.REF", header, value=T)
-  
-  ids = id.line %>%
-    gsub("\"", "", .) %>%
-    gsub("\'", "", .) %>%
-    strsplit("\t") %>%
-    .[[1]]
-  
-  gsms = gsm.line %>%
-    gsub("\"", "", .) %>%
-    gsub("\'", "", .) %>%
-    strsplit("\t") %>%
-    .[[1]]
-  
-  ind = f(id, ids)
-  if(length(ind) > 1) stop("Multiple matches for ID found")
-  
-  data.frame(series.id=gse.id,
-             gsm.id=gsms[[ind]],
-             raw.id=id)
-  
-}
+files = list.files("~/data/methyl_age/GEO/GSM", full.names=T)
 
+reg = makeRegistry("mapping", 
+                   packages=c("dplyr", "stringr"),
+                   src.files="mapper_funcs.R")
+batchMap(reg, gse.mapper, files)
