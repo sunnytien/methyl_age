13d3f3cf89374c8b4ccf21878f53fe28fbca8e8e
Eric Kramer <ekramer@zeus.geh.pasteur.fr>
2015-04-01 16:00:26 +0200

    making ancestry PCA

--- ancestry_prediction/ancestry_pca.R
+++ ancestry_prediction/ancestry_pca.R
@@ -2,6 +2,7 @@ library("dplyr")
 library("randomForest")
 library("tidyr")
 library("pcaMethods")
+library("ggvis")
 
 load("./data/sample.info.Rdata")
 load("./data/rf.Rdata")
@@ -19,9 +20,62 @@ b.matrix = b %>%
   select(-Probe) %>%
   as.matrix %>%
   t 
+colnames(b.matrix) = b$Probe
+
+m.matrix = log(b.matrix / (1 - b.matrix))
 
 good.samples = sample.info %>% 
   filter(!(tissue %in% c("Lymphocytes", "Lymphoblasts"))) %>%
-  filter(gsm.ind %in% rownames(b.matrix))
+  filter(gsm.id %in% rownames(b.matrix))
 
-p = pca(b.matrix[good.samples$gsm.id,1:100], center=T, scale="none")
+m.p = pca(m.matrix[good.samples$gsm.id, sample(1:ncol(b.matrix), 1000)], 
+          center=T, 
+          scale="none",
+          nPcs=4)
+
+b.p = pca(b.matrix[good.samples$gsm.id, sample(1:ncol(b.matrix), 1000)], 
+          center=T, 
+          scale="none",
+          nPcs=4)
+
+
+m.p.scores = m.p %>%
+  scores %>%
+  as.data.frame %>%
+  mutate(gsm.id=rownames(.)) %>%
+  inner_join(sample.info)
+
+b.p.scores = b.p %>%
+  scores %>%
+  as.data.frame %>%
+  mutate(gsm.id=rownames(.)) %>%
+  inner_join(sample.info)
+
+imp = rf %>%
+  importance %>%
+  as.data.frame %>%
+  mutate(gsm.id=rownames(.)) %>%
+  arrange(-MeanDecreaseGini) %>%
+  head(100) %>%
+  .$gsm.id
+
+b.p.imp = pca(b.matrix[good.samples$gsm.id, imp], 
+          center=T, 
+          scale="none",
+          nPcs=4)
+
+b.p.imp.scores = b.p.imp %>%
+  scores %>%
+  as.data.frame %>%
+  mutate(gsm.id=rownames(.)) %>%
+  inner_join(sample.info)
+
+plot(b.p.imp.scores$PC1,
+     col=factor(b.p.imp.scores$ancestry))
+
+b.p.imp.scores %>%
+  ggvis(~PC1, ~PC2, fill:="grey",opacity:=0.2) %>%
+  layer_points() %>%
+  add_data(b.p.imp.scores %>%
+             filter(!is.na(ancestry))) %>%
+  layer_points(fill:=~factor(ancestry), opacity:=1)
