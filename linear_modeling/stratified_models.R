795ed07aa4dc7e4c663895fcd7c2ec840bd155bc
Eric Kramer <ekramer@zeus.geh.pasteur.fr>
2015-04-23 14:31:06 +0200

    horvath ages

--- linear_modeling/stratified_models.R
+++ linear_modeling/stratified_models.R
@@ -1,5 +1,7 @@
 library("BatchJobs")
 
+load("./data/horvath_ages.Rdata")
+
 source("./linear_modeling/stratified_model_funcs.R")
 source("./linear_modeling/util.R")
 
@@ -20,6 +22,6 @@ model.data.files = list.files("./data/model_data", full.names=T) %>%
 reg = makeRegistry("models2", src.files=c("./linear_modeling/util.R",
                                           "./linear_modeling/stratified_model_funcs.R"))
 
-batchMap(reg, run.models2, model.data.files, )
+batchMap(reg, run.models2, model.data.files, more.args=list(horvath_ages=horvath_ages))
 
 submitJobs(reg, chunk(findNotSubmitted(reg), n.chunks=150))
