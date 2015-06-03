4c4527225c7ddae3984e3d8889920eebba7cba4e
Eric Kramer <ericransomkramer@gmail.com>
2015-06-03 11:49:04 +0200

    read me file

--- preprocessing/mappers.R
+++ preprocessing/mappers.R
@@ -4,15 +4,6 @@ library("magrittr")
 source("mapper_funcs.R")
 
 files = list.files("~/data/methyl_age/GEO/GSM", full.names=T)
-# 
-# reg = makeRegistry("mapping", 
-#                    packages=c("dplyr", "stringr"),
-#                    src.files="mapper_funcs.R")
-# batchMap(reg, gse.mapper, files)
-# 
-# submitJobs(reg, 1:10)
-# submitJobs(reg, chunk(findNotSubmitted(reg),
-#                       n.chunks=30))
 
 gse.mapper.safe = failwith(data.frame(series.id=NA,
                                       gsm.id=NA,
@@ -25,5 +16,3 @@ mapping = files %>%
   group_by(filename) %>%
   do(gse.mapper.safe(.$filename)) %>%
   filter(!is.na(series.id))
-
-files = list.files("./data/GSE40279/GSM", full.names=T)
